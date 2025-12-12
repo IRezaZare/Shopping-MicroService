f037e300129e2c9f99c4ce76b1a68c0298eb6cdf
Reza Zare <rezasingle.org@gmail.com>
2025-12-12 22:31:49 +0330

    Add query handling and mapping for products and types
    Updated `Catalog.Application.csproj` to include new folder structures for product and type queries, and added package references for `AutoMapper` and `MediatR`.
    
    Introduced `ProfileMapper` for mapping domain entities to response models. Created query classes and handlers for retrieving product types, brands, and products, along with their corresponding response classes (`BrandResponse`, `ProductResponse`, and `TypeResponse`).

---
+++ services/Catalog.Application/Mapper/ProfileMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+
+namespace Catalog.Application.Mapper;
+
+public class ProfileMapper : Profile
+{
+    public ProfileMapper()
+    {
+        CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+        CreateMap<ProductType, TypeResponse>().ReverseMap();
+        CreateMap<Product, ProductResponse>().ReverseMap();
+    }
+}
---
+++ services/Catalog.Application/Queries/GetAllProductTypeQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Queries;
+
+public class GetAllProductTypeQuery : IRequest<IEnumerable<TypeResponse>>
+{
+
+}
+public class GetAllProductTypeQueryHandler(ITypeRepository repository , IMapper mapper) : IRequestHandler<GetAllProductTypeQuery , IEnumerable<TypeResponse>>
+{
+    public async Task<IEnumerable<TypeResponse>> Handle(GetAllProductTypeQuery request, CancellationToken cancellationToken)
+    {
+        var result = await repository.GetProductTypes();
+        return mapper.Map<IEnumerable<TypeResponse>>(result);
+    }
+}
