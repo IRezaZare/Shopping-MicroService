c8f33812608c895b6bbe114111429a226a2785ca
Reza Zare <rezasingle.org@gmail.com>
2026-01-01 14:54:43 +0330

    Add API versioning and expand CatalogController
    Updated `Catalog.Api.csproj` to include package references for API versioning. Added a new `ApiController` class for versioning support. Expanded `CatalogController` with multiple CRUD methods for product and brand management, utilizing MediatR for command and query handling. Fixed typos in method names for consistency.

---
+++ services/Catalop.Api/Controllers/ApiController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class ApiController : ControllerBase;
---
+++ services/Catalop.Api/Controllers/CatalogController.cs
@@ -0,0 +1,69 @@
+﻿using Catalog.Application.Commands.Products;
+using Catalog.Application.Queries;
+using Catalog.Application.Queries.Brands;
+using Catalog.Application.Queries.Products;
+using Catalog.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+public class CatalogController(IMediator mediator) : ApiController
+{
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductResponse>> GetProductById(string id, CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new GetProductByIdQuery(id), cancellationToken));
+    }
+    [HttpGet("{name}")]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GetProductByName(string name, CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new GetProductsByNameQuery(name), cancellationToken));
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GetAllProducts()
+    {
+        return Ok(await mediator.Send(new GetAllProductsQuery()));
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<BrandResponse>>> GetAllBrands(CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new GetAllProductBrandsQuery(), cancellationToken));
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TypeResponse>>> GetAllTypes(CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new GetAllProductTypeQuery(), cancellationToken));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GatProductsByBrandName(string brand ,
+        CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new GetProductsByBrandQuery(brand), cancellationToken));
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GatProductsByTypeName(string type ,
+        CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new GetProductByTypeQuery(type), cancellationToken));
+    }
+    [HttpPost]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> CreateProduct([FromBody] CreateProductCommand command,
+        CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(command, cancellationToken));
+    }
+    [HttpPut]
+    public async Task<ActionResult<bool>> UpdateProduct([FromBody] UpdateProductCommand command,
+        CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(command, cancellationToken));
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(string id , CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new DeleteProductCommand(id), cancellationToken));
+    }
+}
